ad0878d9b51f569227a6f377e21444d84b004549
Dennis Murage <dmurage140@gmail.com>
2023-03-22 22:52:43 +0300

    Fixed typo

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-import models
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
-    """
-    """
-    __tablename__ = "amenities"
+    """ amenity table declarative class """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    place_amenties = relationship('Place', secondary='place_amenity', viewonly=False)
