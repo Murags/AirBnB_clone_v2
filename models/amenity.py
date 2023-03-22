2b5e4bc47faae9182da7c9eb246506cfb74d9505
Dennis Murage <dmurage140@gmail.com>
2023-03-22 18:27:22 +0300

    Class file updates

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+import models
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary='place_amenity')
