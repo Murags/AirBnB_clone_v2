31332223b27cc9df08d8b92d1244e89080b0852f
Dennis Murage <dmurage140@gmail.com>
2023-03-22 23:44:37 +0300

    tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,87 +1,23 @@
 #!/usr/bin/python3
-""" test amenity file """
-import unittest
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-from models.base_model import BaseModel
-from sqlalchemy.orm.collections import InstrumentedList
-import models
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
-import os
-from datetime import datetime
-from models.base_model import Base
-from sqlalchemy.orm import sessionmaker
-from time import sleep
-from tests.test_models.test_base_model import TestBaseModel
-NoneType = type(None)
+from os import getenv
+from unittest import skipIf
 
 
-class TestAmenity(unittest.TestCase):
+@skipIf(getenv('HBNB_TYPE_STORAGE') == 'db', "Tests for file storage")
+class test_Amenity(test_basemodel):
     """ """
 
-    @classmethod
-    def setUpClass(cls):
-        """executes after all tests"""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        FileStorage._FileStorage__objects = {}
-
-    @classmethod
-    def tearDownClass(cls):
-        """executes after all tests"""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-
-    def setUp(self):
-        """executes after a test"""
-        pass
-
-    def tearDown(self):
-        """executes before a test"""
-        pass
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
 
     def test_name2(self):
         """ """
-        new = Amenity()
-        self.assertTrue(hasattr(new, "name"))
-        self.assertEqual(type(new.name), NoneType)
-
-    def test_place_amenities(self):
-        """ """
-        new = Amenity()
-        self.assertTrue(hasattr(new, "place_amenities"))
-        self.assertEqual(type(new.place_amenities), InstrumentedList)
-
-    def test_instance_attributes(self):
-        """test for attributes."""
-        am = Amenity()
-        self.assertTrue(hasattr(am, "__tablename__"))
-        self.assertTrue(hasattr(am, "name"))
-        self.assertTrue(hasattr(am, "place_amenities"))
-
-    def test_inherits_BaseModel(self):
-        """test if subclass of BaseModel."""
-        self.assertTrue(issubclass(Amenity, BaseModel))
-
-    def test_init_args_kwargs(self):
-        """Test initialization with args and kwargs."""
-        dt = datetime.utcnow()
-        bm = Amenity(id="345", created_at=dt.isoformat())
-        self.assertEqual(bm.id, "345")
-        self.assertEqual(bm.created_at, dt)
-
-    @unittest.skipIf(type(models.storage) is DBStorage, "Filestorage")
-    def test_save_Amenity(self):
-        """test if the save works"""
-        am = Amenity()
-        sleep(0.6)
-        am.save()
-        self.assertNotEqual(am.created_at, am.updated_at)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        new = self.value()
+        new.name = "Wi-Fi"
+        self.assertEqual(type(new.name), str)
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,160 +1,83 @@
 #!/usr/bin/python3
-""" test place file """
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
-import unittest
-from models.city import City
-from models.base_model import BaseModel
-from sqlalchemy.orm.collections import InstrumentedList
-from datetime import datetime
-import models
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
-import os
-import pep8
-from time import sleep
-from tests.test_models.test_base_model import TestBaseModel
-NoneType = type(None)
+from os import getenv
+from unittest import skipIf
 
 
-class TestPlace(unittest.TestCase):
+@skipIf(getenv('HBNB_TYPE_STORAGE') == 'db', "Tests for file storage")
+class test_Place(test_basemodel):
     """ """
 
-    @classmethod
-    def setUpClass(cls):
-        """executes after all tests"""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        FileStorage._FileStorage__objects = {}
-
-    @classmethod
-    def tearDownClass(cls):
-        """executes after all tests"""
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-
-    def setUp(self):
-        """executes after a test"""
-        pass
-
-    def tearDown(self):
-        """executes before a test"""
-        pass
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Place"
+        self.value = Place
 
     def test_city_id(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.city_id), NoneType)
+        new = self.value()
+        new.city_id = "123456789"
+        self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.user_id), NoneType)
+        new = self.value()
+        new.user_id = "123456789"
+        self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.name), NoneType)
+        new = self.value()
+        new.name = "Tiny Home"
+        self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.description), NoneType)
+        new = self.value()
+        new.description = "Best staycation location"
+        self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.number_rooms), NoneType)
+        new = self.value()
+        new.number_rooms = 2
+        self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.number_bathrooms), NoneType)
+        new = self.value()
+        new.number_bathrooms = 3
+        self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.max_guest), NoneType)
+        new = self.value()
+        new.max_guest = 4
+        self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.price_by_night), NoneType)
+        new = self.value()
+        new.price_by_night = 50
+        self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.latitude), NoneType)
+        new = self.value()
+        new.latitude = 111.901
+        self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
-        new = Place()
-        self.assertEqual(type(new.latitude), NoneType)
+        new = self.value()
+        new.longitude = 234.908
+        self.assertEqual(type(new.longitude), float)
 
     def test_amenity_ids(self):
         """ """
-        new = Place()
+        new = self.value()
+        new.price_by_night = ["1234", "5678", "9101112"]
         self.assertEqual(type(new.amenity_ids), list)
-
-    def test_pep8(self):
-        """Test pep8 styling."""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(["models/place.py"])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_docstrings(self):
-        """Check for docstrings."""
-        self.assertIsNotNone(Place.__doc__)
-
-    def test_attributes(self):
-        """test attributes"""
-        pl = Place()
-        self.assertEqual(str, type(pl.id))
-        self.assertEqual(datetime, type(pl.created_at))
-        self.assertEqual(datetime, type(pl.updated_at))
-        self.assertTrue(hasattr(pl, "__tablename__"))
-        self.assertTrue(hasattr(pl, "city_id"))
-        self.assertTrue(hasattr(pl, "name"))
-        self.assertTrue(hasattr(pl, "description"))
-        self.assertTrue(hasattr(pl, "number_rooms"))
-        self.assertTrue(hasattr(pl, "number_bathrooms"))
-        self.assertTrue(hasattr(pl, "max_guest"))
-        self.assertTrue(hasattr(pl, "price_by_night"))
-        self.assertTrue(hasattr(pl, "latitude"))
-        self.assertTrue(hasattr(pl, "longitude"))
-
-    def test_is_subclass(self):
-        """Check that Place is a subclass of BaseModel."""
-        self.assertTrue(issubclass(Place, BaseModel))
-
-    def test_init(self):
-        """Test initialization."""
-        pl = Place()
-        self.assertIsInstance(pl, Place)
-
-    @unittest.skipIf(type(models.storage) is DBStorage, "Filestorage")
-    def test_save_filestorage(self):
-        """Test save """
-        pl = Place()
-        sleep(0.6)
-        pl.save()
-        self.assertNotEqual(pl.created_at, pl.updated_at)
-
-    def test_to_dict(self):
-        """Test to_dict method."""
-        pl = Place()
-        place_dict = pl.to_dict()
-        self.assertEqual(dict, type(place_dict))
-        self.assertEqual(pl.id, place_dict["id"])
-        self.assertEqual("Place", place_dict["__class__"])
-        self.assertEqual(pl.created_at.isoformat(),
-                         place_dict["created_at"])
-        self.assertEqual(pl.updated_at.isoformat(),
-                         place_dict["updated_at"])
-
-
-if __name__ == "__main__":
-    unittest.main(defaultTest="TestBaseModel", exit=False)
