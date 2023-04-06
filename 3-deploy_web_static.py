718bc5093c81f74be9b87121344e8b06e902edcc
Dennis Murage <dmurage140@gmail.com>
2023-04-06 14:10:04 +0300

    added 3-deploy_web_static.py

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Creates an archive for the web_static folder"""
+from fabric.api import local
+from datetime import datetime
+from fabric.api import run, put, env, with_settings
+
+
+env.hosts = ['54.144.131.23', '34.207.221.231']
+
+
+@runs_once
+@with_settings(warn_only=True)
+def do_pack():
+    """Executes commands locally to create archive"""
+    try:
+        time = datetime.now()
+        str_time = time.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        archive_name = "versions/web_static_{}.tgz".format(str_time)
+        execute = local("tar -czvf {} web_static".format(archive_name))
+        return archive_name
+    except Exception:
+        return None
+
+
+@with_settings(warn_only=True)
+def do_deploy(archive_path):
+    """ships and unpacks the .tgv file"""
+    file_name = archive_path.split('/')[-1]
+    folder_extract = file_name.replace(".tgz", "")
+
+    put(archive_path, '/tmp')
+    run('mkdir -p /data/web_static/releases/{}'.format(folder_extract))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(file_name, folder_extract))
+
+    run('rm /tmp/{}'.format(file_name))
+    run('mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(folder_extract, folder_extract))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_extract))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_extract))
+
+def deploy():
+    archive_name = do_pack()
+
+    if archive_name == None:
+        return False
+    else
+        return do_deploy(archive_name)
