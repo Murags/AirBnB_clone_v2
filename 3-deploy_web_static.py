ec291f85550adc76a5ac2c0dd4778cad76c4511e
Dennis Murage <dmurage140@gmail.com>
2023-04-06 14:22:44 +0300

    added 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,6 +39,8 @@ def do_deploy(archive_path):
     run('rm -rf /data/web_static/current')
     run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_extract))
 
+
+@task
 def deploy():
     """_summary_
 
